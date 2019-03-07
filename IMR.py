ead4e6e707e05571ab4401b60b3d31911dc2c8f1
Jasmina Jovanovic <jasminajancic@gmail.com>
2019-03-06 21:00:58 -0600

    added analysis for causes by age and african american highest and lowest IMR counties

--- IMR.py
+++ IMR.py
@@ -48,7 +48,7 @@ dfImrByRace['expected'] = dfImrByRace['Death Rate'].mean()
 dfImrByRace.head()
 
 
-# In[9]:
+# In[67]:
 
 
 dfImrByCountyByRace = pd.read_csv("./IMR by county by race, 2007-2016.txt", sep='\t')
@@ -56,26 +56,30 @@ dfImrByCountyByRace.dropna(subset=['Race'], inplace=True)
 dfImrByCountyByRace.shape
 
 
-# In[10]:
+# In[70]:
 
 
 # remove rows with unreliable death rate data (fewer than 20 deaths)
 dfImrByCountyByRace = dfImrByCountyByRace.loc[dfImrByCountyByRace['Death Rate'].map(lambda x: 'Unreliable' not in str(x))]
+# dfImrByCountyByRace.loc[dfImrByCountyByRace['Race'] is np.nan]
+# todo: include uknown race
+# dfImrByCountyByRace['Race'] = ["Unknown" if race is np.nan else race for race in dfImrByCountyByRace['Race']]
+
 # convert death rate to float
 dfImrByCountyByRace['Death Rate'] = dfImrByCountyByRace['Death Rate'].map(lambda x: float(x))
-dfImrByCountyByRace.head()
 
 
-# In[11]:
+# In[71]:
 
 
 blacks = dfImrByCountyByRace.loc[dfImrByCountyByRace['Race Code'] == '2054-5']['Death Rate']
 natives = dfImrByCountyByRace.loc[dfImrByCountyByRace['Race Code'] == '1002-5']['Death Rate']
 whites = dfImrByCountyByRace.loc[dfImrByCountyByRace['Race Code'] == '2106-3']['Death Rate']
 asians = dfImrByCountyByRace.loc[dfImrByCountyByRace['Race Code'] == 'A-PI']['Death Rate']
+unknown = dfImrByCountyByRace.loc[dfImrByCountyByRace['Race'] == 'Unknown']['Death Rate']
 
 
-# In[12]:
+# In[72]:
 
 
 dfImrByCountyByRace.boxplot('Death Rate', by="Race", figsize=(20, 10))
@@ -86,6 +90,43 @@ dfImrByCountyByRace.boxplot('Death Rate', by="Race", figsize=(20, 10))
 
 # ANOVA shows that one (or more) race(s) is significantly different than the rest
 stats.f_oneway(blacks, natives, whites, asians)
+
+
+# In[309]:
+
+
+dfImrByCountyByRace.loc[(dfImrByCountyByRace['Race'] == "Black or African American") & (dfImrByCountyByRace['County'] == 'Allen County, IN')]
+
+
+# In[310]:
+
+
+dfImrByCountyByRace.loc[(dfImrByCountyByRace['Race'] == "Black or African American") & (dfImrByCountyByRace['County'] == 'Santa Clara County, CA')]
+
+
+# In[331]:
+
+
+blacksByCounty = dfImrByCountyByRace.set_index(['Race', 'County']).sort_values(['Race', 'Death Rate'], ascending=False).loc['Black or African American']
+# groupedByRace.sort_values(['Race', 'Death Rate'], ascending=False).loc['Black or African American']
+blacksByCounty = blacksByCounty.loc[blacksByCounty['Death Rate'].notnull()]
+# for county in blacksByCounty.index:
+#     blacksByCounty
+# blacksByCounty
+
+
+# In[340]:
+
+
+blacksHighestImrCounties = blacksByCounty.head(10)
+blacksHighestImrCounties.to_csv("./AfricanAmericanHighestImrCounties.csv")
+
+
+# In[339]:
+
+
+blacksLowestImrCounties = blacksByCounty.tail(10)
+blacksLowestImrCounties.to_csv("./AfricanAmericanLowestImrCounties.csv")
 
 
 # In[14]:
@@ -156,8 +197,85 @@ plt.title ("IMR by Race, 2001-2016")
 # In[ ]:
 
 
-# leading cause of death
+
 # leading cause of death by age
 # leading cause of death by race
 # death by age by race
+# WHO rates by country
+
+
+# In[183]:
+
+
+# Death Rate by age
+dfImrByAgeByCause = pd.read_csv("./IMR by age by cause, 2007-2016.txt", sep='\t')
+dfImrByAgeByCause.sort_values(by='Death Rate', ascending=False).head(30)
+
+
+# In[ ]:
+
+
+dfImrByAgeByCause['Age of Infant at Death'] = ["Unknown" if age is np.nan else age for age in dfImrByAgeByCause['Age of Infant at Death']]
+# dfImrByAgeByCause.dropna(subset=['Age of Infant at Death'], inplace=True)
+dfImrByAgeByCause = dfImrByAgeByCause.loc[dfImrByAgeByCause['Death Rate'].map(lambda x: 'Unreliable' not in str(x))]
+#convert death rate to float
+dfImrByAgeByCause['Death Rate'] = dfImrByAgeByCause['Death Rate'].map(lambda x: float(x))
+# dfImrByAgeByCause.sort_values(by=['Death Rate'], ascending = False).head(20)
+
+
+# In[248]:
+
+
+dfTotalsbyAgebyCause = dfImrByAgeByCause.loc[(dfImrByAgeByCause['Notes']=='Total') & (dfImrByAgeByCause['Age of Infant at Death'].notnull()) ]
+
+
+# In[258]:
+
+
+# dfTotalsbyAgebyCause['Death Rate']
+dfTotalsbyAgebyCause['Death Rate'] = [float(rate) for rate in dfTotalsbyAgebyCause['Death Rate']]
+
+
+# In[259]:
+
+
+plt.barh(dfTotalsbyAgebyCause['Age of Infant at Death'], width=dfTotalsbyAgebyCause['Death Rate'])
+# plt.barh(temp['Age of Infant at Death'], width=temp['Death Rate'])
+# plt.yticks(dfTotalsbyAgebyCause['Age of Infant at Death'], dfTotalsbyAgebyCause['Age of Infant at Death'], rotation="vertical")
+# plt.tight_layout
+# plt.xlim(0, 5)
+plt.title("IMR vs Age")
+plt.ylabel("Age of Infant at Death")
+plt.xlabel("Death Rate")
+plt.show()
+
+
+# In[255]:
+
+
+# removes rows that show total
+dfImrByAgeByCause = dfImrByAgeByCause.loc[(dfImrByAgeByCause['Notes'] != 'Total')].sort_values(by=['Death Rate'], ascending=False).head(20)
+
+
+# In[256]:
+
+
+infantsUnder1Hour = dfImrByAgeByCause.loc[dfImrByAgeByCause['Age of Infant at Death'] == 'Under 1 hour'].sort_values(by='Death Rate', ascending=False).head(3)
+infantsUnder1Day = dfImrByAgeByCause.loc[dfImrByAgeByCause['Age of Infant at Death'] == '1 - 23 hours'].sort_values(by='Death Rate', ascending=False).head(3)
+infants1To6Days = dfImrByAgeByCause.loc[dfImrByAgeByCause['Age of Infant at Death'] == '1 - 6 days'].sort_values(by='Death Rate', ascending=False).head(3)
+infants7To27Days = dfImrByAgeByCause.loc[dfImrByAgeByCause['Age of Infant at Death'] == '7 - 27 days'].sort_values(by='Death Rate', ascending=False).head(3)
+infants1monthTo1Year = dfImrByAgeByCause.loc[dfImrByAgeByCause['Age of Infant at Death'] == '28 - 364 days'].sort_values(by='Death Rate', ascending=False).head(3)
+
+
+# In[257]:
+
+
+groupedByAgeTop3 = dfImrByAgeByCause.sort_values(by=['Age of Infant at Death', 'Death Rate'], ascending=False).groupby('Age of Infant at Death').head(3)
+groupedByAgeTop3.head()
+
+
+# In[ ]:
+
+
+
 
